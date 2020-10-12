769d737576f0808917e92cdf281d90c74449cc50
Nikita Tomilov <programmer174@icloud.com>
2020-10-12 18:50:50 +0300

    main_for_analytics: bag of words

--- main_for_analytics.py
+++ main_for_analytics.py
@@ -1,12 +1,68 @@
 #!/usr/bin/env python3
+import os
+import re
+from collections import Counter
+
+import nltk
+from nltk.corpus import stopwords
+
 from repository import *
 from uni_list import *
+
+nltk.download("stopwords")
+
+mystem_executable_path = "/opt/mystem/mystem"
+tmp_review_file = "/tmp/review.txt"
+tmp_review_mystemed_file = "/tmp/review.txt.mystemed"
+tmp_all_reviews_file = "/tmp/all_reviews.txt"
+russian_stopwords = stopwords.words("russian")
+russian_custom_stopwords = ["весь", "это", "очень", "который", "студент", "вуз", "преподаватель", "мочь", "курс",
+                            "человек", "университет", "учиться", "факультет"]
+stopwords = russian_stopwords + russian_custom_stopwords
+
+
+def append_file(path, text):
+    text_file = open(path, "a")
+    text_file.write("\n\n")
+    text_file.write(text)
+    text_file.close()
+
+
+def write_file(path, text):
+    text_file = open(path, "w")
+    text_file.write(text)
+    text_file.close()
+
+
+def read_file(path):
+    file = open(path, 'r')
+    all_of_it = file.read()
+    file.close()
+    return all_of_it
 
 
 def parse_date(date):
     locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
     result = datetime.datetime.strptime(date, u'%Y-%m-%d')
     return result
+
+
+def bag_of_words(reviews):
+    os.remove(tmp_all_reviews_file)
+    for review in reviews:
+        write_file(tmp_review_file, review.text)
+        os.system(mystem_executable_path + " " + tmp_review_file + " -l >" + tmp_review_mystemed_file)
+        mystemed_text = read_file(tmp_review_mystemed_file)
+        mystemed_cleared_text = re.sub(r'{(.+?)}', '\\1 ', mystemed_text)
+        mystemed_cleared_text = re.sub(r'[|].*?[ ]', ' ', mystemed_cleared_text)
+        mystemed_cleared_text = re.sub(r'[?]', '', mystemed_cleared_text)
+        append_file(tmp_all_reviews_file, mystemed_cleared_text)
+    data_set = read_file(tmp_all_reviews_file)
+    split_it = data_set.split()
+    split_it = list(filter(lambda x: x not in stopwords, split_it))
+    counter = Counter(split_it)
+    most_occur = counter.most_common(20)
+    print(most_occur)
 
 
 if __name__ == '__main__':
@@ -49,6 +105,12 @@ if __name__ == '__main__':
                 mark_avg = 0
             print(mark_avg, "|", sep='', end='')
         print()
+
+    print("\n")
+    for uni_idx in range(0, len(university_list_tabiturient)):
+        reviews = get_for_uni(conn, cursor, uni_idx)
+        print("Bag of words for uni", university_list_tabiturient[uni_idx])
+        bag_of_words(reviews)
 
     close(conn)
     print("done")

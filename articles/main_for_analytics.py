d09476ba92793274605adff2c9082ca79c78e978
Nikita Tomilov <programmer174@icloud.com>
2020-12-23 19:36:31 +0300

    articles: add main_for_analytics

---
+++ articles/main_for_analytics.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import sqlite3
+
+university_list = (
+    "mifi", "mipt", "hse",
+    "itmo", "spbstu", "eltech",
+    "urfu", "susu", "kantiana",
+    "dvfu", "sfu", "nsu", "tpu",
+    "extra_msu", "extra_baumanka", "extra_spbsu", "extra_tsu"
+)
+
+
+def init(dbfilename: str):
+    conn = sqlite3.connect(dbfilename)
+    cursor = conn.cursor()
+    return conn, cursor
+
+
+def get_articles_count(conn, cursor, uni, year):
+    cursor.execute("""
+                SELECT article_count FROM ArticlesCount WHERE university LIKE ? AND article_year = ?;
+            """, (uni,year,))
+    rows = cursor.fetchall()
+    ans = 0
+    for i in range(0, len(rows)):
+        ans = rows[i][0]
+    return ans
+
+
+if __name__ == '__main__':
+    years = range(2000, 2020)
+    conn, cursor = init("db.sqlite")
+
+    print("Articles per year:")
+    print("uni,", end='')
+    for year in years:
+        print(year, ",", sep='', end='')
+    print()
+
+    for uni_idx in range(0, len(university_list)):
+        uni = university_list[uni_idx]
+        print(university_list[uni_idx] + ",", end='')
+        for year in years:
+            articles_count = get_articles_count(conn, cursor, uni, year)
+            print(articles_count, ",", sep='', end='')
+        print()
+
+    print("done")
